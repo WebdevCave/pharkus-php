8527d93b3f4155c66ad8b5fea48bd4ed2ea91c49
Carlos Alberto B. Carucce <carlos.carucce@weareglobals.com>
2024-06-10 16:23:56 -0300

    Creating Produces and Consumes annotation

---
+++ src/Annotations/Consumes.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Webdevcave\Pharkus\Annotations;
+
+use Attribute;
+use Webdevcave\Pharkus\Media\MediaType;
+
+#[Attribute(Attribute::TARGET_METHOD | Attribute::TARGET_FUNCTION)]
+class Consumes
+{
+    public readonly string $mediaType;
+
+    public function __construct(
+        MediaType|string $mediaType
+    )
+    {
+        if ($mediaType instanceof MediaType) {
+            $mediaType = $mediaType->value;
+        }
+
+        $this->mediaType = $mediaType;
+    }
+}
